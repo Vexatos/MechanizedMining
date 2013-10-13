34c1c508645529bb45419218bc6ed6b3d3a4daeb
Archtikz <atroglyphics@gmail.com>
2013-10-13 11:48:49 +0100

    Testing new scanner code

--- src/dark/mining/common/machines/scanner/TileEntityScanner.java
+++ src/dark/mining/common/machines/scanner/TileEntityScanner.java
@@ -9,65 +9,61 @@ import dark.core.prefab.machine.TileEntityEnergyMachine;
 import dark.mining.common.privateutils.TripleContainer;
 
 /** @author Archadia */
-public class TileEntityScanner extends TileEntityEnergyMachine
-{
+public class TileEntityScanner extends TileEntityEnergyMachine {
 
     private ArrayList<Integer> validTarget = new ArrayList<Integer>();
     private TripleContainer<Integer, Integer, Integer> usedTarget = new TripleContainer<Integer, Integer, Integer>();
 
     public boolean enabled = true;
 
-    Vector3[] vex = new Vector3[3];
+    Vector3[] vec3 = new Vector3[15];
     
-    public TileEntityScanner()
-    {
+    public TileEntityScanner() {
         super(0.5f); //500W
         
     }
 
     @Override
-    public void updateEntity()
-    {
-        super.updateEntity();
-        if (!worldObj.isRemote)
-        {
-            for (byte i = 0; i < 5 && this.canFunction(); i++)
-            {
+    public void updateEntity() {
+    	super.updateEntity();
+    	
+        if (!worldObj.isRemote) {
+            for (byte i = 0; i < 5 && this.canFunction(); i++) {
+            	System.out.println("Hello come again!");
                 scanArea();
             }
         }
     }
 
     @Override
-    public boolean canFunction()
-    {
+    public boolean canFunction() {
         return super.canFunction() && this.enabled;
     }
 
     /** Scans the area for valid blocks */
-    protected void scanArea()
-    {
-        int targetX = xCoord - 150;
-        int targetZ = zCoord - 150;
-        int targetY = this.worldObj.rand.nextInt(15) + 5;
-
-        targetX += (this.worldObj.rand.nextInt(300));
-        targetZ += (this.worldObj.rand.nextInt(300));
-
-        int targetId = worldObj.getBlockId(targetX, targetY, targetZ);
-
-        if (!usedTarget.getAList().contains(targetX) && !usedTarget.getBList().contains(targetY) && !usedTarget.getCList().contains(targetZ))
-        {
-            if (isTargetValid(targetId))
-            {
-                printResult(targetId);
-                usedTarget.put(targetX, targetY, targetZ);
-            }
-        }
+    protected void scanArea() {    	
+    	int firstY = this.yCoord - 1;
+    	int firstZ = this.zCoord - 8;
+    	
+    	int curZ = 0;
+    	
+    	for(int i = 0; i < vec3.length; i++) {
+    		curZ = (int) vec3[i].z;
+    		if(curZ < firstZ + 15) {
+	    		vec3[i] = new Vector3((this.xCoord - 8) + i, firstY, firstZ);
+	    		if(isTargetValid(vec3[i].getBlockID(worldObj))) {
+	    			printResult(vec3[i].getBlockID(worldObj));
+	    		}
+	    		vec3[i].translate(new Vector3(0, 0, 1));
+    		} else {
+    			vec3[i].translate(new Vector3(0, 1, 0));
+    			return;
+    		}
+    	}
+    	
     }
 
-    protected boolean isTargetValid(int id)
-    {
+    protected boolean isTargetValid(int id) {
         for (Block block : Block.blocksList)
         {
             if (block.blockID == id)
@@ -80,14 +76,12 @@ public class TileEntityScanner extends TileEntityEnergyMachine
         return false;
     }
 
-    protected void printResult(int id)
-    {
+    protected void printResult(int id) {
         System.out.println("Found " + id);
     }
 
     @Override
-    public boolean canConnect(ForgeDirection dir)
-    {
+    public boolean canConnect(ForgeDirection dir) {
         return true;
     }
 
