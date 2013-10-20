ce129b9252e761056415e5c3b891164e6cc6e03d
Archtikz <atroglyphics@gmail.com>
2013-10-20 13:11:28 +0100

    Texture Change

--- src/dark/mining/common/block/BlockMM.java
+++ src/dark/mining/common/block/BlockMM.java
@@ -4,17 +4,14 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.Icon;
-import net.minecraftforge.common.Configuration;
-import dark.core.common.DMCreativeTab;
 import dark.core.prefab.machine.BlockMachine;
 import dark.mining.common.privateutils.ModConfig;
 
 /** @author Archadia */
 public class BlockMM extends BlockMachine
 {
-    private Icon[] icons;
+    private Icon[] icons = new Icon[0];
     private String iconPath;
-    private boolean isMultiTextured = false;
 
     public BlockMM(String blockName, Material material)
     {
@@ -36,34 +33,34 @@ public class BlockMM extends BlockMachine
     protected void setIconMax(int max)
     {
         icons = new Icon[max];
-        isMultiTextured = true;
     }
 
     @Override
     public void registerIcons(IconRegister ir)
     {
-        if (!isMultiTextured)
-        {
-            this.blockIcon = ir.registerIcon("mechanizedmining:" + iconPath);
-        }
-        else
-        {
-            this.icons[0] = ir.registerIcon("mechanizedmining:MM_mechanizedBlock_side");
-            this.icons[1] = ir.registerIcon("mechanizedmining:MM_mechanizedBlock_top");
-        }
+    	switch(icons.length) {
+	    	case 0:
+	            this.blockIcon = ir.registerIcon("mechanizedmining:" + iconPath);
+	            break;
+	    	case 2:
+	    		this.icons[0] = ir.registerIcon("mechanizedmining:MM_mechanizedBlock_side");
+	            this.icons[1] = ir.registerIcon("mechanizedmining:MM_mechanizedBlock_top");
+	            break;
+    	}
     }
 
     @Override
-    public Icon getIcon(int side, int meta)
-    {
-        if (isMultiTextured)
-        {
-            if (side > 1)
-            {
-                return icons[0];
-            }
-            return icons[1];
-        }
-        return this.blockIcon;
+    public Icon getIcon(int side, int meta) {
+    	switch(icons.length) {
+	    	case 0:
+	            return this.blockIcon;
+	    	case 2:
+	            if (side > 1) {
+	                return icons[0];
+	            }
+	            return icons[1];
+	    	default:
+	    		return this.blockIcon;
+    	}
     }
 }
