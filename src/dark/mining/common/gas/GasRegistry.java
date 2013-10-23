bc22ad0d635a0260ecd712dda7b9991345a11eda
Archtikz <atroglyphics@gmail.com>
2013-10-24 00:20:32 +0100

    Finished GasRegistry, so far.

--- src/dark/mining/common/gas/GasRegistry.java
+++ src/dark/mining/common/gas/GasRegistry.java
@@ -11,7 +11,7 @@ import com.google.common.collect.HashBiMap;
  */
 public class GasRegistry {
 
-	static int idLimit = 0;
+	static int currentID = 0;
 	
 	static HashMap<String, Gas> storedGas = new HashMap<String, Gas>();
     static BiMap<String, Integer> gasIDs = HashBiMap.create();
@@ -21,8 +21,19 @@ public class GasRegistry {
     		return false;
     	}
     	storedGas.put(gas.getName(), gas);
-    	gasIDs.put(gas.getName(), idLimit++);
+    	gasIDs.put(gas.getName(), currentID++);
     	return true;
     }
+
+    public Gas getGas(String name) {
+    	return storedGas.get(name);
+    }
     
+    public String getGasName(int id) {
+    	return gasIDs.inverse().get(id);
+    }
+    
+    public int getGasID(String name) {
+    	return gasIDs.get(name);
+    }
 }
