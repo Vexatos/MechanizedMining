4d6128916549700a37ff5d70c161dcc5dee65e61
Robert <rseifert.phone@gmail.com>
2013-11-10 20:36:56 -0500

    Fixed Render
    You forgot to pushMatrix

--- src/dark/mining/client/render/RenderFrackingPipe.java
+++ src/dark/mining/client/render/RenderFrackingPipe.java
@@ -7,23 +7,25 @@ import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 import dark.core.prefab.ModPrefab;
+import dark.mining.MechanizedMining;
 import dark.mining.client.model.ModelFrackingPipe;
 
-/**
- * @author Archadia
- *
- */
-public class RenderFrackingPipe extends TileEntitySpecialRenderer {
-	
-	public static final ModelFrackingPipe model = new ModelFrackingPipe();
+/** @author Archadia */
+public class RenderFrackingPipe extends TileEntitySpecialRenderer
+{
+
+    public static final ModelFrackingPipe model = new ModelFrackingPipe();
     public static final String TEXTURE = "frackingpipe.png";
 
-	public void renderTileEntityAt(TileEntity tileEntity, double d, double d1, double d2, float f) {
-		bindTexture(new ResourceLocation("mechanizedmining", ModPrefab.MODEL_DIRECTORY + TEXTURE));
-		GL11.glTranslatef((float) d + 0.5F, (float) d1 + 1.5F, (float) d2 + 0.5F);
-		GL11.glScalef(1.0F, -1F, -1F);
-		
-		model.renderAll();
-		GL11.glPopMatrix(); // end
-	}
+    public void renderTileEntityAt(TileEntity tileEntity, double d, double d1, double d2, float f)
+    {
+        bindTexture(new ResourceLocation(MechanizedMining.instance.DOMAIN, ModPrefab.MODEL_DIRECTORY + TEXTURE));
+
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float) d + 0.5F, (float) d1 + 1.5F, (float) d2 + 0.5F);
+        GL11.glScalef(1.0F, -1F, -1F);
+
+        model.renderAll();
+        GL11.glPopMatrix(); // end
+    }
 }
