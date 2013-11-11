22a76bbd970ca8cca88bd6b76fe00dc3ecfc040e
Archtikz <atroglyphics@gmail.com>
2013-11-11 00:48:34 +0000

    Update

--- src/dark/mining/block/BlockFrackingPipe.java
+++ src/dark/mining/block/BlockFrackingPipe.java
@@ -1,6 +1,12 @@
 package dark.mining.block;
 
+import java.util.List;
+
+import com.builtbroken.common.Pair;
+
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
 
 /**
  * @author Archadia
@@ -11,4 +17,8 @@ public class BlockFrackingPipe extends BlockMM {
 	public BlockFrackingPipe() {
 		super("Fracking_Pipe", Material.wood);
 	}
+	
+    public void getClientTileEntityRenderers(List<Pair<Class<? extends TileEntity>, TileEntitySpecialRenderer>> list) {
+    	
+    }
 }
