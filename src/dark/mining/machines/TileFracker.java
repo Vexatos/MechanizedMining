07738b442f609fd5ba452ff17df87a8f633e61db
Archtikz <atroglyphics@gmail.com>
2013-11-11 00:04:56 +0000

    Updated Staging

--- src/dark/mining/machines/TileFracker.java
+++ src/dark/mining/machines/TileFracker.java
@@ -11,8 +11,7 @@ public class TileFracker extends TileEntityEnergyMachine
 {
 
     private Vector3 target;
-    private boolean flag1 = false;
-    private boolean flag2 = false;
+    private boolean flag = false;
     
     public static final float STAGE_1_USAGE = 0.5f;
     public static final float STAGE_2_USAGE = 0.5f;
@@ -29,9 +28,8 @@ public class TileFracker extends TileEntityEnergyMachine
         super.updateEntity();
         if (!worldObj.isRemote) {
         	if(this.ticks % 20 == 0) {
-        		if(!flag1) clearArea();
-        		if(flag1) deployPipe();
-    			if(flag2) collect();
+        		if(!flag) clearArea();
+    			if(flag) collect();
         	}
     	}
     }
@@ -52,35 +50,13 @@ public class TileFracker extends TileEntityEnergyMachine
     				 this.consumePower(STAGE_1_USAGE, true);
     			 }
 			}
-			flag1 = false;
+			flag = false;
 		} else {
-			flag1 = true;
+			flag = true;
 			target = null;
 		}
     }
-    
-    public void deployPipe() {
-    	if(target == null) {
-    		target = new Vector3(xCoord, yCoord, zCoord);
-    	}
-    	if(target.intY() > 0) {
-			if(this.getEnergyStored() >= STAGE_2_USAGE) {
-				target.translate(Vector3.DOWN());
-	
-	   			int blockID = target.getBlockID(this.worldObj);
-	   			Block block = Block.blocksList[blockID];
-	   			if (block != null && !block.isAirBlock(this.worldObj, target.intX(), target.intY(), target.intZ()) && block.getBlockHardness(this.worldObj, target.intX(), target.intY(), target.intZ()) >= 0) {
-	   				worldObj.setBlock(target.intX(), target.intY(), target.intZ(), MechanizedMining.frackingPipe.blockID);
-	   				this.consumePower(STAGE_2_USAGE, true);
-	   			}
-			}
-			flag2 = false;
-    	} else {
-    		flag2 = true;
-    		target = null;
-    	}
-    }
-    
+
     public void collect() {
     	if(target == null) {
     		target = new Vector3(xCoord, yCoord, zCoord);
@@ -93,15 +69,16 @@ public class TileFracker extends TileEntityEnergyMachine
 	   			Block block = Block.blocksList[blockID];
 	   			if (block != null && !block.isAirBlock(this.worldObj, target.intX(), target.intY(), target.intZ()) && block.getBlockHardness(this.worldObj, target.intX(), target.intY(), target.intZ()) >= 0) {
 	   				if(block instanceof BlockGasOre) {
+
 		   				worldObj.setBlockToAir(target.intX(), target.intY(), target.intZ());
 		   				this.consumePower(STAGE_4_USAGE, true);
 		   				//add gas to container
 	   				}
+	   				worldObj.setBlock(target.intX(), target.intY(), target.intZ(), MechanizedMining.frackingPipe.blockID);
+	   				this.consumePower(STAGE_2_USAGE, true);
 	   			}
 			}
-			flag2 = false;
     	} else {
-    		flag2 = true;
     		target = null;
     	}
     }
