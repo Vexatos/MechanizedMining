cd76926c45cb23f679fac6059a9a8d20fa6f8f05
Archtikz <atroglyphics@gmail.com>
2013-11-12 18:05:36 +0000

    Actually, going to approach it differently

--- src/dark/mining/machines/tile/TileFracker.java
+++ src/dark/mining/machines/tile/TileFracker.java
@@ -8,71 +8,66 @@ import dark.mining.MMRecipeLoader;
 import dark.mining.MechanizedMining;
 
 /** @author Archadia */
-public class TileFracker extends TileEntityEnergyMachine {
+public class TileFracker extends TileEntityEnergyMachine
+{
 
-	private Vector3[] clearingTarget = new Vector3[9];
-	private Vector3[] pipeTarget = new Vector3[9];
+    private Vector3 target, target2;
+    
+    public static final float STAGE_1_USAGE = 0.5f;
+    public static final float STAGE_2_USAGE = 0.5f;
+    public static final float STAGE_3_USAGE = 2.0f;
+    public static final float STAGE_4_USAGE = 4.0f;
 
-	public static final float STAGE_1_USAGE = 0.5f;
-	public static final float STAGE_2_USAGE = 0.5f;
-	public static final float STAGE_3_USAGE = 2.0f;
-	public static final float STAGE_4_USAGE = 4.0f;
+    public TileFracker()
+    {
+        super(0, 5);
+    }
 
+    @Override
+    public void updateEntity() {
+        super.updateEntity();
+        if (!worldObj.isRemote) {
+        	if(this.ticks % 20 == 0) {
+        		//clearArea();
+    			collect();
+        	}
+    	}
+    }
+    
+    /** Returns if its finished its job */
+    public void clearArea() {
+		if (target == null) {
+			target = new Vector3(xCoord, yCoord, zCoord);
+		}
+		if(target.intY() > 0) {
+			if(this.getEnergyStored() >= STAGE_1_USAGE) {
+    			 target.translate(Vector3.DOWN());
 
-	public TileFracker() {
-		super(0, 5);
-	}
-
-	@Override
-	public void updateEntity() {
-		super.updateEntity();
-		if (!worldObj.isRemote) {
-			if (this.ticks % 20 == 0) {
-				collect();
+    			 int blockID = target.getBlockID(this.worldObj);
+    			 Block block = Block.blocksList[blockID];
+    			 if (block != null && !block.isAirBlock(this.worldObj, target.intX(), target.intY(), target.intZ()) && block.getBlockHardness(this.worldObj, target.intX(), target.intY(), target.intZ()) >= 0) {
+    				 worldObj.setBlockToAir(target.intX(), target.intY(), target.intZ());
+    				 this.consumePower(STAGE_1_USAGE, true);
+    			 }
 			}
 		}
-	}
+    }
 
-	/** Returns if its finished its job */
-	public void clearArea() {
-		if (clearingTarget[0] == null) {
-			clearingTarget[0] = new Vector3(xCoord, yCoord, zCoord);
-		}
-		if (clearingTarget[0].intY() > 0) {
-			if (this.getEnergyStored() >= STAGE_1_USAGE) {
-				clearingTarget[0].translate(Vector3.DOWN());
-
-				int blockID = clearingTarget[0].getBlockID(this.worldObj);
-				Block block = Block.blocksList[blockID];
-				if (block != null
-						&& !block.isAirBlock(this.worldObj, clearingTarget[0].intX(),
-								clearingTarget[0].intY(), clearingTarget[0].intZ())
-						&& block.getBlockHardness(this.worldObj, clearingTarget[0].intX(),
-								clearingTarget[0].intY(), clearingTarget[0].intZ()) >= 0) {
-					worldObj.setBlockToAir(clearingTarget[0].intX(), clearingTarget[0].intY(),
-							clearingTarget[0].intZ());
-					this.consumePower(STAGE_1_USAGE, true);
-				}
+    public void collect() {
+    	if(target2 == null) {
+    		target2 = new Vector3(xCoord, yCoord, zCoord);
+    	}
+    	if(target2.intY() > 0) {
+			if(this.getEnergyStored() >= STAGE_2_USAGE) {
+				target2.translate(Vector3.DOWN());
+	
+	   			int blockID = target2.getBlockID(this.worldObj);
+	   			Block block = Block.blocksList[blockID];
+	   			if(block == null) {
+	   				worldObj.setBlock(target2.intX(), target2.intY(), target2.intZ(), MMRecipeLoader.frackingPipe.blockID);
+	   				this.consumePower(STAGE_2_USAGE, true);
+	   			}
 			}
-		}
-	}
-
-	public void collect() {
-		if (pipeTarget[0] == null) {
-			pipeTarget[0] = new Vector3(xCoord, yCoord, zCoord);
-		}
-		if (pipeTarget[0].intY() > 0) {
-			if (this.getEnergyStored() >= STAGE_2_USAGE) {
-				pipeTarget[0].translate(Vector3.DOWN());
-
-				int blockID = pipeTarget[0].getBlockID(this.worldObj);
-				Block block = Block.blocksList[blockID];
-				if (block == null) {
-					worldObj.setBlock(pipeTarget[0].intX(), pipeTarget[0].intY(),
-							pipeTarget[0].intZ(), MMRecipeLoader.frackingPipe.blockID);
-					this.consumePower(STAGE_2_USAGE, true);
-				}
-			}
-		}
-	}
+    	}
+    }
 }
