766215e4d445ca16f0dc064df92508f2b3b21661
Archtikz <atroglyphics@gmail.com>
2013-11-13 23:22:40 +0000

    Now collects gas!

--- src/dark/mining/machines/tile/TileFracker.java
+++ src/dark/mining/machines/tile/TileFracker.java
@@ -1,7 +1,13 @@
 package dark.mining.machines.tile;
 
 import net.minecraft.block.Block;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
 import universalelectricity.core.vector.Vector3;
+import dark.core.common.DarkMain;
+import dark.core.common.blocks.BlockGasOre;
+import dark.core.prefab.fluids.EnumGas;
+import dark.core.prefab.fluids.GasTank;
 import dark.core.prefab.machine.TileEntityEnergyMachine;
 import dark.mining.MMRecipeLoader;
 
@@ -9,12 +15,9 @@ import dark.mining.MMRecipeLoader;
 public class TileFracker extends TileEntityEnergyMachine
 {
 
-    private Vector3 target, target2;
-    
-    public static final float STAGE_1_USAGE = 0.5f;
-    public static final float STAGE_2_USAGE = 0.5f;
-    public static final float STAGE_3_USAGE = 2.0f;
-    public static final float STAGE_4_USAGE = 4.0f;
+	GasTank tank = new GasTank(10000);
+	
+    private Vector3 target;
 
     public TileFracker()
     {
@@ -26,46 +29,32 @@ public class TileFracker extends TileEntityEnergyMachine
         super.updateEntity();
         if (!worldObj.isRemote) {
         	if(this.ticks % 20 == 0) {
-        		//clearArea();
-    			//collect();
+        		frack();
+        		System.out.println("Amt: " + tank.getFluidAmount());
         	}
     	}
     }
  
-    /** Returns if its finished its job */
-    public void clearArea() {
+    public void frack() {
 		if (target == null) {
 			target = new Vector3(xCoord, yCoord, zCoord);
 		}
 		if(target.intY() > 0) {
-			if(this.getEnergyStored() >= STAGE_1_USAGE) {
+			if(this.getEnergyStored() >= 4) {
     			 target.translate(Vector3.DOWN());
 
     			 int blockID = target.getBlockID(this.worldObj);
     			 Block block = Block.blocksList[blockID];
-    			 if (block != null && !block.isAirBlock(this.worldObj, target.intX(), target.intY(), target.intZ()) && block.getBlockHardness(this.worldObj, target.intX(), target.intY(), target.intZ()) >= 0) {
+    			 if (block != null) {
+    				 if(block instanceof BlockGasOre) {
+    					 tank.fill(new FluidStack((Fluid) EnumGas.NATURAL_GAS.getGas(), 1000), true);
+    				 }
     				 worldObj.setBlockToAir(target.intX(), target.intY(), target.intZ());
-    				 this.consumePower(STAGE_1_USAGE, true);
+        			 this.consumePower(2, true);
     			 }
+    			 worldObj.setBlock(target.intX(), target.intY(), target.intZ(), MMRecipeLoader.frackingPipe.blockID);
+    			 this.consumePower(0.5f, true);
 			}
 		}
-    }
-
-    public void collect() {
-    	if(target2 == null) {
-    		target2 = new Vector3(xCoord, yCoord, zCoord);
-    	}
-    	if(target2.intY() > 0) {
-			if(this.getEnergyStored() >= STAGE_2_USAGE) {
-				target2.translate(Vector3.DOWN());
-	
-	   			int blockID = target2.getBlockID(this.worldObj);
-	   			Block block = Block.blocksList[blockID];
-	   			if(block == null) {
-	   				worldObj.setBlock(target2.intX(), target2.intY(), target2.intZ(), MMRecipeLoader.frackingPipe.blockID);
-	   				this.consumePower(STAGE_2_USAGE, true);
-	   			}
-			}
-    	}
     }
 }
