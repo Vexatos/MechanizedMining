7883b535c9155d619f49feb84b0786b8b85e8cf2
Archtikz <atroglyphics@gmail.com>
2013-12-03 07:20:23 +0000

    Start on GasBurner

---
+++ src/dark/mining/machines/tile/TileGasBurner.java
@@ -0,0 +1,61 @@
+package dark.mining.machines.tile;
+
+import java.util.HashSet;
+
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidHandler;
+import dark.core.prefab.fluids.Gas;
+import dark.core.prefab.fluids.GasTank;
+import dark.core.prefab.machine.TileEntityMachine;
+
+/**
+ * @author Archadia
+ *
+ */
+public class TileGasBurner extends TileEntityMachine implements IFluidHandler {
+
+	GasTank tank = new GasTank(5000);
+	HashSet<Fluid> validFuel = new HashSet<Fluid>();
+	
+	public boolean isGasValidFuel(Fluid fluid) {
+		if(!fluid.isGaseous()) return false;
+		if(!validFuel.contains(fluid)) return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int fill(ForgeDirection from, FluidStack resource, boolean doFill) {
+		return 0;
+	}
+
+	@Override
+	public FluidStack drain(ForgeDirection from, FluidStack resource,
+			boolean doDrain) {
+		return null;
+	}
+
+	@Override
+	public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain) {
+		return null;
+	}
+
+	@Override
+	public boolean canFill(ForgeDirection from, Fluid fluid) {
+		return false;
+	}
+
+	@Override
+	public boolean canDrain(ForgeDirection from, Fluid fluid) {
+		return false;
+	}
+
+	@Override
+	public FluidTankInfo[] getTankInfo(ForgeDirection from) {
+		return null;
+	}
+	
+}
